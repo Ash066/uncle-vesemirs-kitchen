cdcf38eab89e5f7fb6dfdfd38d6a815465d7acdf
Ash066 <aisling.o.neill066@gmail.com>
2021-02-11 15:40:44 +0000

    Edit recipe page added. Crud functionality and associated buttons added for appropriate users

--- app.py
+++ app.py
@@ -90,9 +90,10 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username").capitalize()))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username").capitalize()))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
